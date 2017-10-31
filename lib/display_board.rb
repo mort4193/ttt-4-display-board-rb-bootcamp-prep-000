ea1a9f95b20c9f73ae5d56413c5fd873a235a096
jonathan Morton <jmorton4193@gmail.com>
2017-10-31 02:26:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(x)
-board = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
-pos = board["#{x}"]
+board= ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
+pos = board+"#{x}"
 puts pos
 end
